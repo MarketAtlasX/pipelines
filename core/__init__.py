e3f11fa431a5e34e2cef8e492cb9903320288d54
AnonymousMickey <120690347+AnonymousMickey@users.noreply.github.com>
2026-06-18 21:04:42 +0530

    feat: add core module init

---
+++ core/__init__.py
@@ -0,0 +1,14 @@
+from pipelines.core.base import Pipeline, PipelineStage
+from pipelines.core.types import PipelineType, PipelineStatus, Event, Context, Outcome
+from pipelines.core.state import PipelineState
+
+__all__ = [
+    "Pipeline",
+    "PipelineStage",
+    "PipelineType",
+    "PipelineStatus",
+    "Event",
+    "Context",
+    "Outcome",
+    "PipelineState",
+]
